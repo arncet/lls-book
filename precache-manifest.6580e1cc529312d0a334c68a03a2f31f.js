a5abc9cc2890659174a7184e92be4c9f5361d3c2
arncet <arnaud.cetoute@hotmail.fr>
2019-04-18 15:46:30 +0200

    Updates

---
+++ precache-manifest.6580e1cc529312d0a334c68a03a2f31f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8015e8558957ec5de37f",
+    "url": "/static/js/main.89c6016e.chunk.js"
+  },
+  {
+    "revision": "0fe59ead94afa52090c1",
+    "url": "/static/js/2.3cc3ab74.chunk.js"
+  },
+  {
+    "revision": "8015e8558957ec5de37f",
+    "url": "/static/css/main.197da762.chunk.css"
+  },
+  {
+    "revision": "4cc681fae50ce9c543d87deb9dccf96a",
+    "url": "/index.html"
+  }
+];
